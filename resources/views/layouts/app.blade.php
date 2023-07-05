b94b9af0f89436f8dc38bfebd9c531dba4aff4f5
anjali <anjalibasnet19@gmail.com>
2023-07-05 08:54:37 +0545

    message

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,6 +15,8 @@
     <link rel="dns-prefetch" href="//fonts.bunny.net">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
+<link href="{{ asset('css/style.css')}}" rel="stylesheet">
+
     <!-- Scripts -->
 </head>
 <body>
